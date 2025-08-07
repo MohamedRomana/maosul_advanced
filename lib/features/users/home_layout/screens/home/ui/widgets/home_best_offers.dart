29a5710ee1f3b3b3fe84ef64fe2ccd1f07e50254
MohamedRomana <MohamedRomana124@gmail.com>
2025-08-07 16:01:11 +0300

    route best offers/near you to store details

--- lib/features/users/home_layout/screens/home/ui/widgets/home_best_offers.dart
+++ lib/features/users/home_layout/screens/home/ui/widgets/home_best_offers.dart
@@ -2,9 +2,11 @@ import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:maosul_advanced/core/helper/extentions.dart';
 import 'package:maosul_advanced/core/widgets/app_cached.dart';
 import 'package:maosul_advanced/features/users/home_layout/screens/home/logic/cubit/home_cubit.dart';
 import '../../../../../../../core/constants/colors.dart';
+import '../../../../../../../core/routing/routes.dart';
 import '../../../../../../../core/widgets/app_text.dart';
 import '../../../../../../../generated/locale_keys.g.dart';
 import '../../logic/cubit/home_state.dart';
@@ -35,79 +37,91 @@ class HomeBestOffers extends StatelessWidget {
                 padding: EdgeInsets.symmetric(horizontal: 16.w),
                 separatorBuilder: (context, index) => SizedBox(width: 10.w),
                 scrollDirection: Axis.horizontal,
-                itemBuilder: (context, index) => Stack(
-                  children: [
-                    SizedBox(
-                      height: 150.h,
-                      width: 140.w,
-                      child: ClipRRect(
-                        borderRadius: BorderRadius.circular(10.r),
-                        child: AppCachedImage(
-                          image:
-                              cubit.homeData.bestProviders![index].avatar ?? "",
-                          height: 150.h,
-                          width: 140.w,
-                          fit: BoxFit.cover,
+                itemBuilder: (context, index) => InkWell(
+                  splashColor: Colors.transparent,
+                  highlightColor: Colors.transparent,
+                  onTap: () {
+                    context.pushNamed(
+                      Routes.storeName,
+                      arguments: cubit.homeData.bestProviders![index].id,
+                    );
+                  },
+                  child: Stack(
+                    children: [
+                      SizedBox(
+                        height: 150.h,
+                        width: 140.w,
+                        child: ClipRRect(
+                          borderRadius: BorderRadius.circular(10.r),
+                          child: AppCachedImage(
+                            image:
+                                cubit.homeData.bestProviders![index].avatar ??
+                                "",
+                            height: 150.h,
+                            width: 140.w,
+                            fit: BoxFit.cover,
+                          ),
                         ),
                       ),
-                    ),
-                    Container(
-                      height: 150.h,
-                      width: 140.w,
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(10.r),
-                        color: const Color(0xff1C6544).withAlpha(100),
-                      ),
-                    ),
-                    PositionedDirectional(
-                      top: 10.h,
-                      start: 0.w,
-                      child: Container(
-                        height: 21.h,
-                        width: 35.w,
+                      Container(
+                        height: 150.h,
+                        width: 140.w,
                         decoration: BoxDecoration(
-                          color: AppColors.primary,
-                          borderRadius: BorderRadiusDirectional.only(
-                            topEnd: Radius.circular(10.r),
-                            bottomEnd: Radius.circular(10.r),
-                          ),
+                          borderRadius: BorderRadius.circular(10.r),
+                          color: const Color(0xff1C6544).withAlpha(100),
                         ),
-                        child: Center(
-                          child: AppText(
-                            text:
-                                '${cubit.homeData.bestProviders![index].discount}%',
-                            size: 12.sp,
-                            color: Colors.white,
+                      ),
+                      PositionedDirectional(
+                        top: 10.h,
+                        start: 0.w,
+                        child: Container(
+                          height: 21.h,
+                          width: 35.w,
+                          decoration: BoxDecoration(
+                            color: AppColors.primary,
+                            borderRadius: BorderRadiusDirectional.only(
+                              topEnd: Radius.circular(10.r),
+                              bottomEnd: Radius.circular(10.r),
+                            ),
+                          ),
+                          child: Center(
+                            child: AppText(
+                              text:
+                                  '${cubit.homeData.bestProviders![index].discount}%',
+                              size: 12.sp,
+                              color: Colors.white,
+                            ),
                           ),
                         ),
                       ),
-                    ),
-                    PositionedDirectional(
-                      bottom: 13.h,
-                      start: 5.w,
-                      end: 5.w,
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          AppText(
-                            text:
-                                cubit.homeData.bestProviders![index].name ?? "",
-                            size: 12.sp,
-                            fontWeight: FontWeight.w700,
-                            color: Colors.white,
-                          ),
-                          AppText(
-                            lines: 2,
-                            top: 5.h,
-                            text:
-                                " ${cubit.homeData.bestProviders![index].city} - ${cubit.homeData.bestProviders![index].country}",
-                            size: 10.sp,
-                            color: Colors.white,
-                          ),
-                        ],
+                      PositionedDirectional(
+                        bottom: 13.h,
+                        start: 5.w,
+                        end: 5.w,
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            AppText(
+                              text:
+                                  cubit.homeData.bestProviders![index].name ??
+                                  "",
+                              size: 12.sp,
+                              fontWeight: FontWeight.w700,
+                              color: Colors.white,
+                            ),
+                            AppText(
+                              lines: 2,
+                              top: 5.h,
+                              text:
+                                  " ${cubit.homeData.bestProviders![index].city} - ${cubit.homeData.bestProviders![index].country}",
+                              size: 10.sp,
+                              color: Colors.white,
+                            ),
+                          ],
+                        ),
                       ),
-                    ),
-                  ],
+                    ],
+                  ),
                 ),
               ),
             ),
--- lib/features/users/home_layout/screens/home/ui/widgets/home_near_you.dart
+++ lib/features/users/home_layout/screens/home/ui/widgets/home_near_you.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:maosul_advanced/core/helper/extentions.dart';
 import 'package:maosul_advanced/features/users/home_layout/screens/home/logic/cubit/home_cubit.dart';
+import '../../../../../../../core/routing/routes.dart';
 import '../../../../../../../core/widgets/app_cached.dart';
 import '../../../../../../../core/widgets/app_text.dart';
 import '../../logic/cubit/home_state.dart';
@@ -32,83 +34,93 @@ class HomeNearYou extends StatelessWidget {
                 padding: EdgeInsets.symmetric(horizontal: 16.w),
                 separatorBuilder: (context, index) => SizedBox(width: 10.w),
                 scrollDirection: Axis.horizontal,
-                itemBuilder: (context, index) => Stack(
-                  children: [
-                    SizedBox(
-                      height: 120.h,
-                      width: 200.w,
-                      child: ClipRRect(
-                        borderRadius: BorderRadius.circular(10.r),
-                        child: AppCachedImage(
-                          image:
-                              homeCubit.homeData.nearProviders![index].avatar!,
-                          fit: BoxFit.cover,
+                itemBuilder: (context, index) => InkWell(
+                  splashColor: Colors.transparent,
+                  highlightColor: Colors.transparent,
+                  onTap: (){
+                      context.pushNamed(
+                      Routes.storeName,
+                      arguments: homeCubit.homeData.nearProviders![index].id,
+                    );
+                  },
+                  child: Stack(
+                    children: [
+                      SizedBox(
+                        height: 120.h,
+                        width: 200.w,
+                        child: ClipRRect(
+                          borderRadius: BorderRadius.circular(10.r),
+                          child: AppCachedImage(
+                            image:
+                                homeCubit.homeData.nearProviders![index].avatar!,
+                            fit: BoxFit.cover,
+                          ),
                         ),
                       ),
-                    ),
-                    Container(
-                      height: 120.h,
-                      width: 200.w,
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(10.r),
-                        color: const Color(0xff1C6544).withAlpha(100),
+                      Container(
+                        height: 120.h,
+                        width: 200.w,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(10.r),
+                          color: const Color(0xff1C6544).withAlpha(100),
+                        ),
                       ),
-                    ),
-                    PositionedDirectional(
-                      bottom: 10.h,
-                      start: 6.w,
-                      end: 6.w,
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Row(
-                            children: [
-                              SizedBox(
-                                width: 100.w,
-                                child: AppText(
-                                  text: homeCubit
-                                      .homeData
-                                      .nearProviders![index]
-                                      .name!,
-                                  size: 12.sp,
-                                  color: Colors.white,
-                                  fontWeight: FontWeight.w700,
-                                ),
-                              ),
-                              const Spacer(),
-                              Row(
-                                children: [
-                                  AppText(
-                                    text:
-                                        (homeCubit
-                                                .homeData
-                                                .nearProviders![index]
-                                                .rate!)
-                                            .toString(),
-                                    size: 11.sp,
-                                    color: const Color(0xffFC6011),
-                                    end: 3.w,
+                      PositionedDirectional(
+                        bottom: 10.h,
+                        start: 6.w,
+                        end: 6.w,
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Row(
+                              children: [
+                                SizedBox(
+                                  width: 100.w,
+                                  child: AppText(
+                                    text: homeCubit
+                                        .homeData
+                                        .nearProviders![index]
+                                        .name!,
+                                    size: 12.sp,
+                                    color: Colors.white,
                                     fontWeight: FontWeight.w700,
                                   ),
-                                  Icon(
-                                    Icons.star,
-                                    size: 20.sp,
-                                    color: const Color(0xffFC6011),
-                                  ),
-                                ],
-                              ),
-                            ],
-                          ),
-                          AppText(
-                            text:
-                                '${homeCubit.homeData.nearProviders![index].city} - ${homeCubit.homeData.nearProviders![index].country}',
-                            size: 10.sp,
-                            color: Colors.white,
-                          ),
-                        ],
+                                ),
+                                const Spacer(),
+                                Row(
+                                  children: [
+                                    AppText(
+                                      text:
+                                          (homeCubit
+                                                  .homeData
+                                                  .nearProviders![index]
+                                                  .rate!)
+                                              .toString(),
+                                      size: 11.sp,
+                                      color: const Color(0xffFC6011),
+                                      end: 3.w,
+                                      fontWeight: FontWeight.w700,
+                                    ),
+                                    Icon(
+                                      Icons.star,
+                                      size: 20.sp,
+                                      color: const Color(0xffFC6011),
+                                    ),
+                                  ],
+                                ),
+                              ],
+                            ),
+                            AppText(
+                              text:
+                                  '${homeCubit.homeData.nearProviders![index].city} - ${homeCubit.homeData.nearProviders![index].country}',
+                              size: 10.sp,
+                              color: Colors.white,
+                            ),
+                          ],
+                        ),
                       ),
-                    ),
-                  ],
+                    ],
+                  ),
                 ),
               ),
             ),
